f3afdf6a2130edbc7ed031c8d3a693d152935aee
kis <19x92@naver.com>
2021-12-30 07:33:15 +0900

    21년 12월 30일 오전 7시 33분 - 최종 수정

--- src/kr/or/iei/admin/review/controller/AdminReviewDelYNChangeServlet.java
+++ src/kr/or/iei/admin/review/controller/AdminReviewDelYNChangeServlet.java
@@ -57,6 +57,7 @@ public class AdminReviewDelYNChangeServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
+		
 	}
 
 }
